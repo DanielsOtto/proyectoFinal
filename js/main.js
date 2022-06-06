df7d1cab6fdf194953b7891fffec064d4deeca57
otto <ottod1505@gmail.com>
2022-06-06 15:07:33 -0300

    UPLOAD V.1.0b funcionamiento correcto de las funciones

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ class Articulo {
         this.color = color;
         this.stock = stock;
         this.precio = precio;
+        this.finalPrice = precio;
     }
     hayStock(cant) {
         this.stock = this.stock - cant;
     }
     precioFinal() {
-        this.precio *= 1.21;
+        // this.precio *= 1.21;
+        this.finalPrice = this.precio * 1.21;
     }
 }
 
@@ -31,24 +33,23 @@ function comprobarStock(stock) {
     } // revisa si hay stock suficiente para realizar la venta ANDA BIEN
 }
 
-
-
-
-function precioTotal(cant, article) { //calcula el total de la operacion al multiplicar el precio final del articulo con la cantidad de articulos a comprar.
+function precioTotal(cant, article) {
+    //calcula el total de la operacion al multiplicar el precio final del articulo con la cantidad de articulos a comprar.
     let precioTotal = 0;
 
-    precioTotal = article.precioFinal * cant;
+    precioTotal = article.finalPrice * cant;
     return precioTotal;
 }
 
 function comprando(article) {
     let cantidad = Number(prompt("Cuantas " + article.tipo + " desea comprar ?"));
     let priceTotal = 0;
+
     console.log(cantidad);
     console.log(article.stock);
-    if (cantidad < article.stock) { // revisa si la cantidad a comprar es mayor al stock del producto
-        article.hayStock(cantidad); // esto si funciona, 
-        priceTotal = precioTotal(cantidad, article); // NO FUNCA
+    if (cantidad <= article.stock) { // revisa si la cantidad a comprar es mayor al stock del producto
+        article.hayStock(cantidad);
+        priceTotal = precioTotal(cantidad, article);
         console.log("sirve precio final ?? " + precioTotal);
         console.log("sirve precio final funcion ? " + precioTotal(cantidad, article));
         return priceTotal;
@@ -59,35 +60,40 @@ function comprando(article) {
 }
 
 function realizarCompra() {
-    let compra = prompt("Que tipo de prenda buscabas ? \nREMERA\nCAMISA\nCAMPERA RUNNING").toUpperCase();
+    let compra = prompt("Que tipo de prenda buscabas ? \n1) REMERA\n2) CAMISA\n3) CAMPERA RUNNING").toUpperCase();
     let answerPrice = 0;
+    let quiero = "";
 
     switch (compra) {
+        case '1':
         case articulo1.tipo:
             if (comprobarStock(articulo1.stock)) {
                 articulo1.precioFinal();
-                alert("Disponemos de:"+"\nProducto: "+articulo1.tipo+"\nCantidad: "+articulo1.stock+"\nColor: "+articulo1.color+"\nTalle: "+articulo1.talle+"\nEl precio por unidad es de: "+articulo1.precio);
-              
-                // a partir de aca anda mal !! REVISAR !!
-                answerPrice = Number(comprando(articulo1));
+                alert("Disponemos de:" + "\nProducto: " + articulo1.tipo + "\nCantidad: " + articulo1.stock + "\nColor: " + articulo1.color + "\nTalle: " + articulo1.talle + "\nEl precio por unidad es de: " + articulo1.precio + ", y con los impuestos queda en: " + articulo1.finalPrice);
+                quiero = prompt("Desea comprar ? \n1- SI\n2- NO").toUpperCase();
+                if((quiero === 'SI')||(quiero == '1')){
+                    answerPrice = Number(comprando(articulo1));
                     console.log("que muestra answerPrice ?" + answerPrice);
-                if (answerPrice > 0) {
-                    alert("El monto a pagar es de " + answerPrice);
+                    if (answerPrice > 0) {
+                        alert("El monto a pagar es de " + answerPrice);
+                    } else {
+                        alert("No se puede realizar la venta, stock insuficiente");
+                    }
                 } else {
-                    alert("No se puede realizar la venta, stock insuficiente");
+                    alert("Usted decidio no comprar nada.");
                 }
-
             } else {
                 alert("No quedan mas " + articulo1.tipo + " en nuestro stock."); // ANDA BIEN
             }
             break;
+        case '2':
         case articulo2.tipo:
             if (comprobarStock(articulo2.stock)) {
                 articulo2.precioFinal();
-                alert("Disponemos de:"+"\nProducto: "+articulo2.tipo+"\nCantidad: "+articulo2.stock+"\nColor: "+articulo2.color+"\nTalle: "+articulo2.talle+"\nEl precio por unidad es de: "+articulo2.precio);
-                quiero = prompt("Desea comprar ? \nSI\nNO").toUpperCase();
-                if (quiero === 'SI') {
-                    answerPrice = comprando(articulo2);
+                alert("Disponemos de:" + "\nProducto: " + articulo2.tipo + "\nCantidad: " + articulo2.stock + "\nColor: " + articulo2.color + "\nTalle: " + articulo2.talle + "\nEl precio por unidad es de: " + articulo2.precio + ", y con los impuestos queda en: " + articulo2.finalPrice);
+                quiero = prompt("Desea comprar ? \n1- SI\n2- NO").toUpperCase();
+                if((quiero === 'SI')||(quiero == '1')){
+                    answerPrice = Number(comprando(articulo2));
                     if (answerPrice > 0) {
                         alert("El monto a pagar es de " + answerPrice);
                     } else {
@@ -100,13 +106,14 @@ function realizarCompra() {
                 alert("No quedan mas " + articulo2.tipo);
             }
             break;
+        case '3':
         case articulo3.tipo:
             if (comprobarStock(articulo3.stock)) {
                 articulo3.precioFinal();
-                alert("Disponemos de:"+"\nProducto: "+articulo3.tipo+"\nCantidad: "+articulo3.stock+"\nColor: "+articulo3.color+"\nTalle: "+articulo3.talle+"\nEl precio por unidad es de: "+articulo3.precio);
-                quiero = prompt("Desea comprar ? \nSI\nNO").toUpperCase();
-                if (quiero === 'SI') {
-                    answerPrice = comprando(articulo3);
+                alert("Disponemos de:" + "\nProducto: " + articulo3.tipo + "\nCantidad: " + articulo3.stock + "\nColor: " + articulo3.color + "\nTalle: " + articulo3.talle + "\nEl precio por unidad es de: " + articulo3.precio + ", y con los impuestos queda en: " + articulo3.finalPrice);
+                quiero = prompt("Desea comprar ? \n1- SI\n2- NO").toUpperCase();
+                if((quiero === 'SI')||(quiero == '1')){
+                    answerPrice = Number(comprando(articulo3));
                     if (answerPrice > 0) {
                         alert("El monto a pagar es de " + answerPrice);
                     } else {
@@ -126,8 +133,7 @@ function realizarCompra() {
 }
 
 do {
-
     realizarCompra();
 
-    seguirComprando = prompt("Desea seguir comprando ?? \nSI \nNO").toUpperCase();
-} while (seguirComprando === 'SI');
+    seguirComprando = prompt("Desea seguir comprando ?? \n1- SI \n2- NO").toUpperCase();
+} while (seguirComprando === 'SI'|| seguirComprando == '1');
