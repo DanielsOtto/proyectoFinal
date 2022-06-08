29102a53a09c21fbc03a7f4ba5e1df596c8ef67e
otto <ottod1505@gmail.com>
2022-06-08 00:38:48 -0300

    UPLOAD Ver. 1.0e Agregadas/Eliminadas/Cambios en funciones, Agregadas nuevas propiedades/métodos en la clase

--- js/main.js
+++ js/main.js
@@ -5,25 +5,30 @@ class Articulo {
         this.modelo = modelo;
         this.talle = talle;
         this.color = color;
+        this.cantidad = stock; 
         this.stock = stock;
         this.precio = precio;
         this.finalPrice = precio;
+        this.multiplePrice = precio; 
         this.newPrice = precio;
     }
-    hayStock(cant) {
-        this.stock = this.stock - cant;
+    hayStock() {
+        this.stock = this.stock - this.cantidad;
     }
     precioFinal() {
-        this.finalPrice = this.precio * 1.21;
+        this.finalPrice = Math.round(this.precio * 1.21);
+    }
+    multiplePriceE(){
+        this.multiplePrice = this.finalPrice * this.cantidad;
     }
     fifteenOff() {
-        this.newPrice = this.finalPrice - (this.finalPrice * 0.10);
+        this.newPrice = Math.round(this.multiplePrice - (this.multiplePrice * 0.10));
     }
     threeWithoutI() {
-        this.newPrice = this.finalPrice / 3;
+        this.newPrice = Math.round(this.multiplePrice / 3);
     }
     twelveWithI() {
-        this.newPrice = (this.finalPrice * 1.10) / 12;
+        this.newPrice = Math.round((this.multiplePrice * 1.10) / 12);
     }
 }
 
@@ -34,12 +39,12 @@ const articulo3 = new Articulo("CAMPERA RUNNING", "skreep", "runner", 'S', "gris
 let seguirComprando = 'NO';
 
 
-function comprobarStock(stock) { 
-    if (stock > 0) {
+function comprobarStock(article) { 
+    if (article.stock > 0) {
         return true;
     } else {
         return false;
-    } // revisa si hay stock suficiente para realizar la venta ANDA BIEN
+    } // revisa si hay stock suficiente para realizar la venta
 }
 
 function mostrarArticulo(article) { 
@@ -48,37 +53,24 @@ function mostrarArticulo(article) {
     // calcula el precio del producto con impuestos y muestra los detalles del producto
 }
 
-function precioTotal(cant, article) { 
-    //calcula el total de la operacion al multiplicar el precio final del articulo con la cantidad de articulos a comprar.
-    let precioTotal = 0;
+function comprando(article) {  
+    article.cantidad = Number(prompt("Cuantas " + article.tipo + " desea comprar ?"));
 
-    precioTotal = article.finalPrice * cant; // aca se puede ahorrar mas codigo?
-    return precioTotal;
-}
-
-function comprando(article) {     
-    let cantidad = Number(prompt("Cuantas " + article.tipo + " desea comprar ?")); // NACE CANTIDAD
-    let priceTotal = 0;
-
-    if (cantidad <= article.stock) { // revisa si la cantidad a comprar es mayor al stock del producto
-        article.hayStock(cantidad);
-        priceTotal = precioTotal(cantidad, article); // aca se puede ahorrar mas codigo?
-        return priceTotal;
+    if (article.cantidad <= article.stock) { // revisa si la cantidad a comprar es menor al stock del producto
+        article.multiplePriceE();
+        return true;
     } else {
         alert("No disponemos de tantos items");
-        return priceTotal;
+        return false;
     }
 }
 
-function quieroComprar(article){
-    let answerPrice = 0;
+function quieroComprar(article){ 
     let quiero = "";
-    // 
-    quiero = prompt("Desea comprar ? \n1- SI\n2- NO").toUpperCase();
-    if ((quiero === 'SI') || (quiero == '1')) {
-        answerPrice = Number(comprando(article)); // aca se puede ahorrar mas codigo?
-        if (answerPrice > 0) {
-            alert("El monto a pagar es de " + answerPrice);
+    quiero = prompt("Desea comprar ? \n1- SI\n2- NO").toUpperCase(); 
+    if ((quiero === 'SI') || (quiero == '1')) {  // averigua si el usuario desea comprar el producto
+        if (comprando(article)) {
+            alert("El monto a pagar es de " + article.multiplePrice);
             opcionesDePago(article);
         } else {
             alert("No se puede realizar la venta, stock insuficiente");
@@ -89,7 +81,7 @@ function quieroComprar(article){
 }
 
 function lessCode(article){
-    if (comprobarStock(article.stock)) { 
+    if (comprobarStock(article)) { // reduce codigo
         mostrarArticulo(article); 
         quieroComprar(article); 
     } else {
@@ -97,42 +89,44 @@ function lessCode(article){
     }
 }
 
-// function opcionesDePago(article) {
-//     let quotaOption = 0;
+function opcionesDePago(article) {
+    let quotaOption = 0;
+    let paymentOption= prompt("Con que desea abonar: \n1) Efectivo (Obtendrás un 10% de descuento)\n2) Crédito (Podrás elegir entre 3 cuotas sin interes ó 12 cuotas con interés(10%))\n3) Cancelar".toUpperCase());
 
-//     let paymentOption= prompt("Con que desea abonar: \n1) Efectivo o Débito (Obtendrás un 10% de descuento)\n2) Visa (Podrás elegir entre 3 cuotas sin interes ó 12 cuotas con interés(10%))\n3) Cancelar".toUpperCase());
-
-//     switch(paymentOption){
-//         case '1':
-//         case 'EFECTIVO':
-//         case 'DÉBITO':
-//             article.fifteenOff();
-//             alert("Has elegido efectivo/débito, el importe a pagar era de: " + article.finalPrice + " y ahora es de: " + article.newPrice + "\n Gracias por su compra !!");
-//             break;
-//         case '2':
-//         case 'VISA':
-//             quotaOption = prompt("Has elegido Visa. Cuantas cuotas deseas? (Ingrese 1, 2, 3) \n\t1- 3 cuotas s/i \n\t2- 12 cuotas c/i (+10%) \n\t3- Salir");
-//             if (quotaOption === 1) {
-//                 article.threeWithoutI();
-//                 alert("Has elegido 3 cuotas sin interés.\nDeberás pagar 3 cuotas de: " + article.newPrice);
-//             } else if (quotaOption === 2) {
-//                 article.twelveWithI();
-//                 alert("Has elegido 12 cuotas con interés.\nDeberás pagar 12 cuotas de: " + article.newPrice);
-//             } else if (quotaOption === 3) {
-//                 alert("Has ingresado la opción 3. Operación cancelada");
-//             } else {
-//                 alert("Opción Incorrecta. Operación cancelada");
-//             }
-//             break;
-//         case '3':
-//         case 'CANCELAR':
-//             alert("Has ingresado Cancelar. La operación se cancela.");
-//             break;
-//         default:
-//             alert("El dato ingresado es incorrecto. Operación cancelada.");
-//             break;
-//     }
-// }
+    switch(paymentOption){ // hay 2 opciones de pago efectivo o Credito
+        case '1':
+        case 'EFECTIVO':
+            article.fifteenOff();
+            alert("Has elegido efectivo/débito, el importe a pagar era de: " + article.multiplePrice + " y ahora es de: " + article.newPrice + "\n Gracias por su compra !!");
+            article.hayStock(article);
+            break;
+        case '2':
+        case 'CREDITO':
+        case 'CRÉDITO':
+            quotaOption = Number(prompt("Has elegido Crédito. Cuantas cuotas deseas? (Ingrese 1, 2, 3) \n\t1- 3 cuotas s/i \n\t2- 12 cuotas c/i (+10%) \n\t3- Salir"));
+            if (quotaOption === 1) {
+                article.threeWithoutI();
+                alert("Has elegido 3 cuotas sin interés.\nDeberás pagar 3 cuotas de: " + article.newPrice);
+                article.hayStock(article);
+            } else if (quotaOption === 2) {
+                article.twelveWithI();
+                alert("Has elegido 12 cuotas con interés.\nDeberás pagar 12 cuotas de: " + article.newPrice);
+                article.hayStock(article);
+            } else if (quotaOption === 3) {
+                alert("Has ingresado la opción 3. Operación cancelada");
+            } else {
+                alert("Opción Incorrecta. Operación cancelada");
+            }
+            break;
+        case '3':
+        case 'CANCELAR':
+            alert("Has ingresado Cancelar. La operación se cancela.");
+            break;
+        default:
+            alert("El dato ingresado es incorrecto. Operación cancelada.");
+            break;
+    }
+}
 
 function realizarPedido() {
     let compra = prompt("Que tipo de prenda buscabas ? \n1) REMERA\n2) CAMISA\n3) CAMPERA RUNNING").toUpperCase();
@@ -158,6 +152,5 @@ function realizarPedido() {
 
 do {
     realizarPedido(); 
-
     seguirComprando = prompt("Desea seguir comprando ?? \n1- SI \n2- NO").toUpperCase();
 } while (seguirComprando === 'SI' || seguirComprando == '1');
