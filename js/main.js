b3fc5df66fc50106aad6b5f183e21ff4f733b4ee
otto <ottod1505@gmail.com>
2022-06-12 01:42:54 -0300

    UPLOAD Ver. 2.0a Agregando nuevas funciones → objetivo hacer un filtro de busqueda

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // hacer 2 variables globales ?? una de cantidad de objetos vendidos, y otra de precioMultiple ??
+
 // no mezclar ingles con español -- se enoja juan 
 
 class Articulo {
@@ -34,58 +35,33 @@ class Articulo {
         this.newPrice = Math.round((this.multiplePrice * 1.10) / 12);
     }
 }
-const productos = [];
+const productos = [];  // este arreglo va a tener los objetos en venta
+let muchosColores = " "; // este arreglo va a concatenar los colores que se pueden elegir de los objetos elegidos
 
-// const articuloR1 = new Articulo("REMERA", "skreep", "manga corta", 'L', "negra", 10, 4000);
-// const articuloR2 = new Articulo("REMERA", "skreep", "manga corta", 'L', "negra", 10, 4000);
-// const articuloR3 = new Articulo("REMERA", "skreep", "manga corta", 'L', "negra", 10, 4000);
-// const articuloR4 = new Articulo("REMERA", "skreep", "manga corta", 'L', "negra", 10, 4000);
-// const articuloC1 = new Articulo("CAMISA", "skreep", "manga carga", 'M', "blanca", 4, 9000);
-// const articuloC2 = new Articulo("CAMISA", "skreep", "manga carga", 'M', "blanca", 4, 9000);
-// const articuloC3 = new Articulo("CAMISA", "skreep", "manga carga", 'M', "blanca", 4, 9000);
-// const articuloC4 = new Articulo("CAMISA", "skreep", "manga carga", 'M', "blanca", 4, 9000);
-// const articuloCR1 = new Articulo("CAMPERA RUNNING", "skreep", "runner", 'S', "gris", 7, 18000);
-// const articuloCR2 = new Articulo("CAMPERA RUNNING", "skreep", "runner", 'S', "gris", 7, 18000);
-// const articuloCR3 = new Articulo("CAMPERA RUNNING", "skreep", "runner", 'S', "gris", 7, 18000);
-// const articuloCR4 = new Articulo("CAMPERA RUNNING", "skreep", "runner", 'S', "gris", 7, 18000);
 
-// productos.push(articuloR1);
-// productos.push(articuloR2);
-// productos.push(articuloR3);
-// productos.push(articuloR4);
-// productos.push(articuloC1);
-// productos.push(articuloC2);
-// productos.push(articuloC3);
-// productos.push(articuloC4);
-// productos.push(articuloCR1);
-// productos.push(articuloCR2);
-// productos.push(articuloCR3);
-// productos.push(articuloCR4);
 
-productos.push(new Articulo("REMERA", "SKREEP", "MANGA CORTA", 'L', "NEGRA", 10, 4000));
-productos.push(new Articulo("REMERA", "SKREEP", "MANGA CORTA", 'M', "ROJA", 10, 4000));
-productos.push(new Articulo("REMERA", "SKREEP", "MANGA CORTA", 'S', "BLANCA", 10, 4000));
-productos.push(new Articulo("REMERA", "SKREEP", "MANGA CORTA", 'S', "BLANCA", 10, 4000));
+productos.push(new Articulo("REMERA", "SKREEP", "MANGA LARGA", 'L', "NEGRA", 10, 4000));
+productos.push(new Articulo("REMERA", "SKREEP", "MANGA CORTA", 'M', "ROJA", 8, 4000));
+productos.push(new Articulo("REMERA", "SKREEP", "MANGA LARGA", 'S', "AZUL", 0, 4000));
+productos.push(new Articulo("REMERA", "SKREEP", "MANGA CORTA", 'S', "BLANCA", 4, 4000));
 productos.push(new Articulo("CAMISA", "SKREEP", "MANGA LARGA", 'L', "GRIS", 4, 9000));
-productos.push(new Articulo("CAMISA", "SKREEP", "MANGA LARGA", 'L', "GRIS", 4, 9000));
-productos.push(new Articulo("CAMISA", "SKREEP", "MANGA LARGA", 'M', "ROJA", 4, 9000));
+productos.push(new Articulo("CAMISA", "SKREEP", "MANGA CORTA", 'L', "AZUL", 2, 9000));
+productos.push(new Articulo("CAMISA", "SKREEP", "MANGA CORTA", 'M', "ROJA", 6, 9000));
 productos.push(new Articulo("CAMISA", "SKREEP", "MANGA LARGA", 'M', "BLANCA", 4, 9000));
 productos.push(new Articulo("CAMPERA RUNNING", "SKREEP", "RUNNER", 'L', "NEGRA", 7, 18000));
-productos.push(new Articulo("CAMPERA RUNNING", "SKREEP", "RUNNER", 'L', "ROJA", 7, 18000));
-productos.push(new Articulo("CAMPERA RUNNING", "SKREEP", "RUNNER", 'M', "AZUL", 7, 18000));
-productos.push(new Articulo("CAMPERA RUNNING", "SKREEP", "RUNNER", 'S', "GRIS", 7, 18000));
-
-let seguirComprando = 'NO';
-
+productos.push(new Articulo("CAMPERA RUNNING", "SKREEP", "RUNNER", 'L', "ROJA", 17, 18000));
+productos.push(new Articulo("CAMPERA RUNNING", "SKREEP", "RUNNER", 'M', "AZUL", 1, 18000));
+productos.push(new Articulo("CAMPERA RUNNING", "SKREEP", "RUNNER", 'S', "GRIS", 3, 18000));
 
 // HAY QUE CAMBIAR TODO XQ AHORA HAY Q TRABAJAR SOBRE ARREGLO, NO SOBRE OBJETOS
 
 
 
 
-function comprobarStock(article) { 
-    if (article.stock > 0) {
+function comprobarStock(arreglo) { 
+    if (arreglo.stock > 0) {
         return true;
+        alert("LLEGUE !");
     } else {
         return false;
     } // revisa si hay stock suficiente para realizar la venta
@@ -124,87 +100,123 @@ function quieroComprar(article){
     }
 }
 
-function lessCode(article){
-    if (comprobarStock(article)) { // reduce codigo
-        mostrarArticulo(article); 
-        quieroComprar(article); 
-    } else {
-        alert("No quedan mas " + article.tipo + " en nuestro stock."); 
-    }
+
+
+// function opcionesDePago(article) {
+//     let quotaOption = 0;
+//     let paymentOption= prompt("Con que desea abonar: \n1) Efectivo (Obtendrás un 10% de descuento)\n2) Crédito (Podrás elegir entre 3 cuotas sin interes ó 12 cuotas con interés(10%))\n3) Cancelar".toUpperCase());
+
+//     switch(paymentOption){ // hay 2 opciones de pago efectivo o Credito
+//         case '1':
+//         case 'EFECTIVO':
+//             article.fifteenOff();
+//             alert("Has elegido efectivo/débito, el importe a pagar era de: " + article.multiplePrice + " y ahora es de: " + article.newPrice + "\n Gracias por su compra !!");
+//             article.hayStock(article);
+//             break;
+//         case '2':
+//         case 'CREDITO':
+//         case 'CRÉDITO':
+//             quotaOption = Number(prompt("Has elegido Crédito. Cuantas cuotas deseas? (Ingrese 1, 2, 3) \n\t1- 3 cuotas s/i \n\t2- 12 cuotas c/i (+10%) \n\t3- Salir"));
+//             if (quotaOption === 1) {
+//                 article.threeWithoutI();
+//                 alert("Has elegido 3 cuotas sin interés.\nDeberás pagar 3 cuotas de: " + article.newPrice);
+//                 article.hayStock(article);
+//             } else if (quotaOption === 2) {
+//                 article.twelveWithI();
+//                 alert("Has elegido 12 cuotas con interés.\nDeberás pagar 12 cuotas de: " + article.newPrice);
+//                 article.hayStock(article);
+//             } else if (quotaOption === 3) {
+//                 alert("Has ingresado la opción 3. Operación cancelada");
+//             } else {
+//                 alert("Opción Incorrecta. Operación cancelada");
+//             }
+//             break;
+//         case '3':
+//         case 'CANCELAR':
+//             alert("Has ingresado Cancelar. La operación se cancela.");
+//             break;
+//         default:
+//             alert("El dato ingresado es incorrecto. Operación cancelada.");
+//             break;
+//     }
+// }
+
+// function lessCode(arreglo){
+//     if (comprobarStock(article)) { // reduce codigo
+//         mostrarArticulo(article); 
+//         quieroComprar(article); 
+//     } else {
+//         alert("No quedan mas " + article.tipo + " en nuestro stock."); 
+//     }
+// }
+
+function busquedaPorTalle(arreglo){
+    let talle = prompt("Que talle buscaba ?\nL\nM\nS").toUpperCase();
+    const otraBusqueda = arreglo.filter((o) => o.talle.includes(talle)); // este arreglo lo utiliza para la busqueda de elementos a partir del talle
+    revisarColores(otraBusqueda);
 }
 
-function opcionesDePago(article) {
-    let quotaOption = 0;
-    let paymentOption= prompt("Con que desea abonar: \n1) Efectivo (Obtendrás un 10% de descuento)\n2) Crédito (Podrás elegir entre 3 cuotas sin interes ó 12 cuotas con interés(10%))\n3) Cancelar".toUpperCase());
+function revisarColores(arreglo){
+    let elegirColor = " ";
 
-    switch(paymentOption){ // hay 2 opciones de pago efectivo o Credito
-        case '1':
-        case 'EFECTIVO':
-            article.fifteenOff();
-            alert("Has elegido efectivo/débito, el importe a pagar era de: " + article.multiplePrice + " y ahora es de: " + article.newPrice + "\n Gracias por su compra !!");
-            article.hayStock(article);
-            break;
-        case '2':
-        case 'CREDITO':
-        case 'CRÉDITO':
-            quotaOption = Number(prompt("Has elegido Crédito. Cuantas cuotas deseas? (Ingrese 1, 2, 3) \n\t1- 3 cuotas s/i \n\t2- 12 cuotas c/i (+10%) \n\t3- Salir"));
-            if (quotaOption === 1) {
-                article.threeWithoutI();
-                alert("Has elegido 3 cuotas sin interés.\nDeberás pagar 3 cuotas de: " + article.newPrice);
-                article.hayStock(article);
-            } else if (quotaOption === 2) {
-                article.twelveWithI();
-                alert("Has elegido 12 cuotas con interés.\nDeberás pagar 12 cuotas de: " + article.newPrice);
-                article.hayStock(article);
-            } else if (quotaOption === 3) {
-                alert("Has ingresado la opción 3. Operación cancelada");
-            } else {
-                alert("Opción Incorrecta. Operación cancelada");
+    if(arreglo.length > 0){
+        // lessCode(arreglo);
+        if(arreglo.length > 1){
+            for(const color of arreglo){
+                muchosColores += "\n" + color.color;
             }
-            break;
-        case '3':
-        case 'CANCELAR':
-            alert("Has ingresado Cancelar. La operación se cancela.");
-            break;
-        default:
-            alert("El dato ingresado es incorrecto. Operación cancelada.");
-            break;
+            let elegirColor = prompt("Cual de los siguientes colores desea ?? \n"+muchosColores).toUpperCase;
+        }else if(arreglo.length == 1){
+            let elegirColor = prompt("Solo disponemos del color: \n"+arreglo.color).toUpperCase;
+        }else{
+            alert("ERROR");
+        }
+    }else{
+        alert("No disponemos de ese articulo");
     }
+
+    const resultado = arreglo.find((el) => el.color === elegirColor);
+    // comprobarStock(resultado);  ESTOY ACA
 }
+
 
 function realizarPedido() {
-    let compra = prompt("Que tipo de prenda buscabas ? \n1) REMERA\n2) CAMISA\n3) CAMPERA RUNNING").toUpperCase();
-
-    const resultado = productos.filter((el) => el.tipo.includes(compra));
-
-    if(resultado.length > 0){
-        resultado.forEach((el)=> alert("Artículos encontrados: "+el.tipo+"\nMarca: "+el.marca+"\nModelo: "+el.modelo+"\nTalle: "+el.talle+"\nColor: "+el.color+"\nStock: "+el.stock+"\nPrecio: "+el.precio));
-    }else{
-        console.log("No disponemos de ese artículo");
-    }
     
-    // switch (compra) {
+    let busqueda = prompt("Que tipo de prenda buscabas ? \n REMERA\n CAMISA\n CAMPERA RUNNING").toUpperCase(); 
+
+    const nuevoArreglo = productos.filter((el) => el.tipo.includes(busqueda));  // este arreglo lo voy a utilizar para la busqueda de elementos a partir del tipo
+
+    if(nuevoArreglo.length > 0){
+        nuevoArreglo.forEach((el)=> alert("Artículos encontrados: "+el.tipo+"\nMarca: "+el.marca+"\nModelo: "+el.modelo+"\nTalle: "+el.talle+"\nColor: "+el.color+"\nStock: "+el.stock+"\nPrecio: "+el.precio));
+        busquedaPorTalle(nuevoArreglo);
+    }else{
+        alert("No disponemos de ese artículo");
+    }
+
+    // let pregunta = prompt("Desea realizar por tipo de producto o por talle ??\n1) Remera - Camisa - Campera Running\n2) L - M - S").toUpperCase();
+
+    // switch(pregunta){
     //     case '1':
-    //     case articulo1.tipo:
-    //         lessCode(articulo1);
+    //     case 'REMERA':
+    //     case 'CAMISA':
+    //     case 'CAMPERA RUNNING':
+    //         busquedaPorTipo();
     //         break;
     //     case '2':
-    //     case articulo2.tipo:
-    //         lessCode(articulo2);
-    //         break;
-    //     case '3':
-    //     case articulo3.tipo:
-    //         lessCode(articulo3);
+    //     case 'L':
+    //     case 'M':
+    //     case 'S':
+    //         busquedaPorTalle();
     //         break;
     //     default:
-    //         alert("Objeto no encontrado.")
+    //         alert("Opción no válida");
     //         break;
     // }
-
-
 }
 
 do {
+    let seguirComprando = 'NO';
+
     realizarPedido(); 
     seguirComprando = prompt("Desea seguir comprando ?? \n1- SI \n2- NO").toUpperCase();
 } while (seguirComprando === 'SI' || seguirComprando == '1');
